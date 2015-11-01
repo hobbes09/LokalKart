9f41f2e95a7799523201fa749c8292e08cedc3d7
hobbes09 <sourin.sutradhar@gmail.com>
2015-11-01 20:40:43 +0530

    Adding loader to get authentication token

--- app/src/main/java/com/lokalkart/activities/SplashScreen.java
+++ app/src/main/java/com/lokalkart/activities/SplashScreen.java
@@ -1,13 +1,18 @@
 package com.lokalkart.activities;
 
-import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.content.Loader;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.lokalkart.R;
+import com.lokalkart.models.entities.AuthenticationTokens;
+import com.lokalkart.services.AuthenticationService;
 import com.lokalkart.utils.GlobalConstants;
 
 
@@ -15,30 +20,18 @@ import com.lokalkart.utils.GlobalConstants;
  * Created by sourin on 18/10/15.
  */
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends AppCompatActivity implements LoaderManager.LoaderCallbacks<AuthenticationTokens>{
+
+    AuthenticationTokens mAuthenticationTokens;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        launchHomeScreenActivity();
+        getSupportLoaderManager().initLoader(getApplicationContext().getResources()
+                .getInteger(R.integer.AUTHENTICATION_LOADER), null, this).forceLoad();
     }
 
-    private void launchHomeScreenActivity() {
-        Thread timer = new Thread(){
-            public void run(){
-                try{
-                    sleep(GlobalConstants.SPLASH_SCREEN_TIMEOUT);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intentHomeActivity = new Intent(SplashScreen.this, HomeScreen.class);
-                    startActivity(intentHomeActivity);
-                }
-            }
-        };
-        timer.start();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,4 +54,49 @@ public class SplashScreen extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    @Override
+    public Loader<AuthenticationTokens> onCreateLoader(int id, Bundle args) {
+        this.mAuthenticationTokens = null;
+        AuthenticationLoader mAuthenticationLoader = new AuthenticationLoader(this);
+        return mAuthenticationLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<AuthenticationTokens> loader, AuthenticationTokens data) {
+        if(data != null){
+            this.mAuthenticationTokens = data;
+            Toast.makeText(getApplicationContext(), "Access token : "+ this.mAuthenticationTokens.getAccessToken(), Toast.LENGTH_SHORT).show();
+            GlobalConstants.accessToken = this.mAuthenticationTokens.getAccessToken();
+            GlobalConstants.refreshToken = this.mAuthenticationTokens.getRefreshToken();
+        }else{
+            Toast.makeText(getApplicationContext(), "Unable to connect to server. Please check your internet connection", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<AuthenticationTokens> loader) {
+        this.mAuthenticationTokens = null;
+    }
+
+
+    private static class AuthenticationLoader extends AsyncTaskLoader<AuthenticationTokens> {
+
+        public AuthenticationLoader(Context context){
+            super(context);
+        }
+
+        @Override
+        public AuthenticationTokens loadInBackground() {
+            AuthenticationTokens mAuthenticationTokens = new AuthenticationTokens();
+            AuthenticationService mAuthenticationService = new AuthenticationService(mAuthenticationTokens);
+            mAuthenticationService.fetchAuthenticationToken();
+            if(mAuthenticationService.getResponseCode() == 200){
+                return mAuthenticationService.getmAuthenticationTokens();
+            }
+            return null;
+        }
+    }
+
 }
