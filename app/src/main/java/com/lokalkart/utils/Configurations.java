fbacd2da36b4d838b4af43eee6530118187d8dd9
hobbes09 <sourin.sutradhar@gmail.com>
2015-11-04 02:02:36 +0530

    configurations

---
+++ app/src/main/java/com/lokalkart/utils/Configurations.java
@@ -0,0 +1,19 @@
+package com.lokalkart.utils;
+
+/**
+ * Created by sourin on 03/11/15.
+ */
+public class Configurations {
+
+    public static String accessToken;
+    public static String refreshToken;
+
+    public static final String baseUrl = "6d2f30da.ngrok.io";
+
+    public static final String userCredentialName = "acme";
+    public static final String userCredentialPassword = "acmesecret";
+
+    public static final String authorizationUsername = "admin";    //From user
+    public static final String authorizationPassword = "admin";    //From user
+
+}
