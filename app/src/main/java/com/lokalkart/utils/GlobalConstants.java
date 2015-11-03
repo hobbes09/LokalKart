7284dae78560a617548fe7a82409ab985ce4dc43
hobbes09 <sourin.sutradhar@gmail.com>
2015-11-04 02:02:47 +0530

    global constants

--- app/src/main/java/com/lokalkart/utils/GlobalConstants.java
+++ app/src/main/java/com/lokalkart/utils/GlobalConstants.java
@@ -11,16 +11,7 @@ public class GlobalConstants {
     public static int NUM_COLUMNS_SUBCATEGORY = 2;
     public static int NUM_ADS = 5;
 
-    public static String accessToken;
-    public static String refreshToken;
 
-    public static final String baseUrl = "1e855492.ngrok.io";
-
-    public static final String userCredentialName = "acme";
-    public static final String userCredentialPassword = "acmesecret";
-
-    public static final String authorizationUsername = "admin";    //From user
-    public static final String authorizationPassword = "admin";    //From user
 
 
 
